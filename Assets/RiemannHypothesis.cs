1f6eb42d2650dbc0e2d4c80f36af6aecd88a1473
Matthew Russell Downey <matthew.r.downey@gmail.com>
2019-07-01 19:45:06 -0400

    Some interesting tests related to the Riemann hypothesis

--- Assets/RiemannHypothesis.cs
+++ Assets/RiemannHypothesis.cs
@@ -1,14 +1,54 @@
-﻿using System.Numerics;
-using UnityEngine;
+﻿using UnityEngine;
+using Meta.Numerics;
+using Meta.Numerics.Functions;
 
 public class RiemannHypothesis : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject zeta_prefab;
+
+    public void Update()
     {
-        Debug.Log("Exact form: " + test_output);
-        Debug.Log("Basic implementation: " + riemann_zeta_function(test_input));
-        Debug.Log("Novel implementation: " + iterative_riemann_zeta_function(test_input));
+        float current_time = Time.time;
+        for (int interval = 1; interval <= 10; interval += 1)
+        {
+            //double input = Mathf.Lerp(last_time, current_time, interval/10f);
+            Vector2 random = Random.insideUnitCircle;
+            Complex output = my_function4(new Complex(random.x, random.y));
+            GameObject spawned_object =GameObject.Instantiate(zeta_prefab, new Vector3((float)output.Re, (float)output.Im, 0), Quaternion.identity);
+            GameObject.Destroy(spawned_object, 3f);
+        }
+        last_time = current_time;
+    }
+    public Complex my_function1(Complex s)
+    {
+        return AdvancedComplexMath.RiemannZeta(AdvancedComplexMath.RiemannZeta(s)) + 0.5f;
+    }
+
+    public Complex my_function2(Complex s)
+    {
+        for (int i = 0; i < 100; i += 1)
+        {
+            s = AdvancedComplexMath.RiemannZeta(s);
+        }
+        return s;
+    }
+
+    public Complex my_function3(Complex s)
+    {
+        for (int i = 0; i < 50; i += 1)
+        {
+            s = AdvancedComplexMath.RiemannZeta(AdvancedComplexMath.RiemannZeta(s)) + 0.52861f;
+        }
+        return s;
+    }
+
+    public Complex my_function4(Complex s)
+    {
+        for (int i = 0; i < 50; i += 1)
+        {
+            s = AdvancedComplexMath.RiemannZeta(s) + 0.5f;
+        }
+        return s;
     }
 
     public Complex riemann_zeta_function(Complex s)
@@ -16,7 +56,7 @@ public class RiemannHypothesis : MonoBehaviour
         Complex result = 0;
         for (int exponent_base = 1; exponent_base < infinity; exponent_base += 1)
         {
-            result += 1/Complex.Pow(exponent_base, s);
+            result += 1/ComplexMath.Pow(exponent_base, s);
         }
         return result;
     }
@@ -24,11 +64,11 @@ public class RiemannHypothesis : MonoBehaviour
     public Complex iterative_riemann_zeta_function(Complex s) // Maybe I built this in the opposite direction on top of my other errors? -- getting NaN
     {
         Complex numerator = 1;
-        Complex denominator = Complex.Pow(1, s);
+        Complex denominator = ComplexMath.Pow(1, s);
         for (int exponent_base = 2; exponent_base < infinity; exponent_base += 1)
         {
-            Complex next_denominator = Complex.Pow(denominator, Complex.Log(exponent_base-1));
-            Complex minor_denominator = Complex.Pow(infinity-1, s);
+            Complex next_denominator = ComplexMath.Pow(denominator, ComplexMath.Log(exponent_base-1));
+            Complex minor_denominator = ComplexMath.Pow(infinity-1, s);
             Complex major_denominator = next_denominator/minor_denominator;
 
             Complex minor_numerator = major_denominator;
@@ -40,9 +80,7 @@ public class RiemannHypothesis : MonoBehaviour
         }
         return numerator / denominator;
     }
-
-    private readonly Complex test_input = new Complex(2, Mathf.PI);
-    private readonly Complex test_output = new Complex(0.7956402067356775174, -0.0942944615085101302365576); // generated from Wolfram Alpha
-
-    private const int infinity = 10;
+    
+    private float last_time = 0;
+    private const int infinity = 100;
 }
